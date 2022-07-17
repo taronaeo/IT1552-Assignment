356196a07acfdbfd4e7bc703799a014a3440700a
Aaron Teo <aaron.teo@riv-alumni.com>
2022-07-17 20:06:41 +0800

    refactor: dashboard

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import { Router } from './router';
+import { Layout } from './components/Layout';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <Router />
+    <Layout>
+      <Router />
+    </Layout>
   </React.StrictMode>,
 );
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,13 +1,34 @@
 import React from 'react';
 
 import { signOut } from '../services';
+import { useAuthState } from '../stores/auth';
 
 const Dashboard: React.FC = () => {
-  return (
-    <div>
-      <h1 className="font-bold">congratz!</h1>
+  const user = useAuthState((state) => state.user);
 
-      <button onClick={signOut}>sign out now</button>
+  return (
+    <div className="w-full max-w-7xl flex flex-col">
+      <div className="w-full flex flex-col">
+        <div className="flex gap-4 justify-between items-center">
+          <h1 className="flex gap-4 items-center text-4xl font-medium">
+            <img
+              src={user?.photoURL as string}
+              alt="Profile Picture"
+              className="w-16 h-16 border-2 rounded-full"
+            />
+
+            {user?.displayName}
+          </h1>
+
+          <div className="flex flex-row gap-6 justify-between items-center">
+            <button
+              className="px-6 py-2 border-2 rounded-full"
+              onClick={signOut}>
+              Sign Out
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
