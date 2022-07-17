ae42607102b0b084e670f4ed6ee6eae09c156fe5
Aaron Teo <aaron.teo@riv-alumni.com>
2022-07-17 23:10:32 +0800

    refactor: clean up sidenav

--- src/components/Sidenav.tsx
+++ src/components/Sidenav.tsx
@@ -1,4 +1,15 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faHome,
+  faIdBadge,
+  faUser,
+  faUserGroup,
+  faCalendarDays,
+  faClock,
+  faGear,
+  faArrowRightFromBracket,
+} from '@fortawesome/free-solid-svg-icons';
 
 type SidenavProps = {
   className: string;
@@ -6,13 +17,14 @@ type SidenavProps = {
 
 const Sidenav: React.FC<SidenavProps> = (props) => (
   <aside {...props}>
-    <div className="px-6 mb-4">
+    <div className="px-6 mt-8 mb-4">
       <ul>
-        <li className="sidenav-list-header">IT1552</li>
         <li className="px-4 py-4 hover:rounded-lg hover:bg-sky-600/25">
+          <FontAwesomeIcon icon={faHome} className="w-4 mr-4" />
           Dashboard
         </li>
         <li className="px-4 py-4 hover:rounded-lg hover:bg-sky-600/25">
+          <FontAwesomeIcon icon={faIdBadge} className="w-4 mr-4" />
           Profile
         </li>
       </ul>
@@ -23,15 +35,19 @@ const Sidenav: React.FC<SidenavProps> = (props) => (
 
       <ul>
         <li className="px-4 py-4 hover:rounded-lg hover:bg-sky-600/25">
+          <FontAwesomeIcon icon={faUser} className="w-4 mr-4" />
           Users
         </li>
         <li className="px-4 py-4 hover:rounded-lg hover:bg-sky-600/25">
+          <FontAwesomeIcon icon={faUserGroup} className="w-4 mr-4" />
           Members
         </li>
         <li className="px-4 py-4 hover:rounded-lg hover:bg-sky-600/25">
+          <FontAwesomeIcon icon={faCalendarDays} className="w-4 mr-4" />
           Events
         </li>
         <li className="px-4 py-4 hover:rounded-lg hover:bg-sky-600/25">
+          <FontAwesomeIcon icon={faClock} className="w-4 mr-4" />
           Participations
         </li>
       </ul>
@@ -44,9 +60,14 @@ const Sidenav: React.FC<SidenavProps> = (props) => (
 
       <ul>
         <li className="px-4 py-4 hover:rounded-lg hover:bg-sky-600/25">
+          <FontAwesomeIcon icon={faGear} className="w-4 mr-4" />
           Settings
         </li>
         <li className="px-4 py-4 hover:rounded-lg hover:bg-sky-600/25">
+          <FontAwesomeIcon
+            icon={faArrowRightFromBracket}
+            className="w-4 mr-4"
+          />
           Sign Out
         </li>
       </ul>
