601243f1b12462575ddedf7fa0e6e1b834ced5bf
Aaron Teo <aaron.teo@riv-alumni.com>
2022-07-29 17:38:13 +0800

    add: action button to manageusers

--- src/pages/ManageUsers/ManageUsers.tsx
+++ src/pages/ManageUsers/ManageUsers.tsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronRight } from '@fortawesome/free-solid-svg-icons';
+
 import { Search } from '../../components';
 import { UsersTable } from '.';
 
@@ -7,7 +10,16 @@ const ManageUsers: React.FC = () => {
     <div className="flex flex-col gap-4 mb-10">
       <h1 className="text-4xl font-bold">Manage Users</h1>
 
-      <Search placeholder="Search Users..." />
+      <div className="flex flex-row justify-between items-center">
+        <div>
+          <button className="flex flex-row items-center gap-2 text-black bg-white border border-gray-300 font-medium rounded-lg text-sm p-2 px-4">
+            Add User
+            <FontAwesomeIcon icon={faChevronRight} className="w-5" />
+          </button>
+        </div>
+
+        <Search className="w-60" placeholder="Search for users..." />
+      </div>
 
       <UsersTable />
     </div>
