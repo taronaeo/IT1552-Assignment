4e5d48f72c3068868bc500127b126f392de55dc5
Aaron Teo <aaron.teo@riv-alumni.com>
2022-07-29 17:37:56 +0800

    refactor: made search more flexible

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -3,23 +3,21 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 
 type SearchProps = {
+  className: string;
   placeholder: string;
 };
 
-const Search: React.FC<SearchProps> = ({ placeholder }) => (
-  <div className="relative">
+const Search: React.FC<SearchProps> = ({ className, placeholder }) => (
+  <div className={['relative', className].join(' ')}>
     <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
       <FontAwesomeIcon icon={faMagnifyingGlass} className="text-black" />
     </div>
     <input
       id="search"
       type="search"
-      className="block p-4 pl-10 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border-gray-300"
+      className="block p-2 pl-10 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border-gray-300 focus:ring-blue-500 focus:border-blue-500"
       placeholder={placeholder}
     />
-    <button className="absolute text-white right-2.5 bottom-2 bg-blue-700 font-medium rounded-lg text-sm px-4 py-2">
-      Search
-    </button>
   </div>
 );
 
